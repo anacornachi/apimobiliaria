5ba30b47b2e5d77b31d63d5439c45e02394d0b50
Ana Cornachi <85295984+anacornachi@users.noreply.github.com>
2022-03-22 16:00:23 -0300

    fix connection db

--- server.js
+++ server.js
@@ -1,24 +1,53 @@
-const dotenv = require('dotenv');
+require('dotenv/config');
 const app = require('./src/config/app.js');
 const routes = require('./src/routes/index.js');
 const sequelize = require('./src/db/connection.js');
 
-dotenv.config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
-
 const port = process.env.PORT || 3000;
 
-try {
-  sequelize.authenticate().then(() => {
-    sequelize.sync().then(() => {
+let dbIsReady = false;
+
+const connectDb = async () => {
+  try {
+    console.log(
+      'Trying to start sequelize at',
+      new Date().toLocaleTimeString()
+    );
+    await sequelize.authenticate();
+
+    console.log('Database is connected');
+    dbIsReady = true;
+  } catch {
+    console.log('Database is not able to connect, trying again...');
+  }
+};
+
+const startServer = async () => {
+  console.log('Trying to start server at', new Date().toLocaleTimeString());
+
+  while (!dbIsReady) {
+    await connectDb();
+  }
+  // const tryConnectDB = setInterval(async () => {
+  //   await connectDb();
+  //   if (dbIsReady) {
+  //     clearInterval(tryConnectDB);
+  //   }
+  // }, 1000);
+
+  if (dbIsReady) {
+    try {
+      await sequelize.sync();
       console.log('Database is sync');
+
       routes(app);
       app.listen(port, () => {
         console.log('Server running on http://localhost:' + port);
       });
-    });
-  });
-} catch (error) {
-  console.log(error);
-}
+    } catch (error) {
+      console.log('#'.repeat(200), error);
+    }
+  }
+};
+
+startServer();
