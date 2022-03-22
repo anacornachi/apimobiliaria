322596ff0af6801d5a218e134e36f340c9343e74
Ana Cornachi <85295984+anacornachi@users.noreply.github.com>
2022-03-22 14:34:55 -0300

    fix: project files

--- src/config/database.js
+++ src/config/database.js
@@ -4,8 +4,8 @@ const config = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
   dialect: 'postgres',
 };
 
--- src/db/connection.js
+++ src/db/connection.js
@@ -1,15 +1,8 @@
 require('dotenv/config');
 const Sequelize = require('sequelize');
+const config = require('../config/database.js');
 
-const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-  }
-);
+const sequelize = new Sequelize(config);
 
 try {
   setTimeout(() => {
